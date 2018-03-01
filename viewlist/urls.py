14e4013168fe953111277cc1f5e32419c5e7a417
gregoryfryns <gregory.fryns@gmail.com>
2018-03-01 01:34:38 +0000

    use generic views

--- viewlist/urls.py
+++ viewlist/urls.py
@@ -5,10 +5,8 @@ from . import views
 app_name = 'viewlist'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
-    path('<int:pk>/edit', views.EditView.as_view(), name='edit'),
-    path('<int:profile_id>/update', views.update_profile, name='update'),
+    path('<int:pk>/', views.ProfileDetail.as_view(), name='detail'),
+    path('<int:pk>/edit', views.ProfileUpdate.as_view(), name='edit'),
+    path('create', views.ProfileCreate.as_view(), name='create'),
     path('<int:profile_id>/recommend', views.recommend, name='recommend'),
-    path('add', views.add_profile_form, name='add'),
-    path('create', views.create_profile, name='create'),
 ]
--- viewlist/views.py
+++ viewlist/views.py
@@ -3,77 +3,61 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
 from django.views import generic
 
-from .models import Profile, Recommendation
+from .models import Profile, Recommendation, COUNTRIES
 
 class IndexView(generic.ListView):
     template_name = 'viewlist/index.html'
-    # context_object_name = 'latest_entries'
 
     def get_queryset(self):
         """Return the last ten published questions."""
-        return Profile.objects.order_by('-publish_date')[:10]
+        return Profile.objects.order_by('-publish_date')
 
-class DetailView(generic.DetailView):
+class ProfileDetail(generic.DetailView):
     model = Profile
-    template_name = 'viewlist/detail.html'
 
 class EditView(generic.DetailView):
     model = Profile
     template_name = 'viewlist/edit.html'
-    
-def add_profile_form(request):
-    return render(request, 'viewlist/add.html')
-    
-def create_profile(request):
-    profile = Profile()
-    try:
-        profile.name = request.POST['name']
-        profile.email = request.POST['email']
-        profile.webpage = request.POST['webpage']
-        profile.institution = request.POST['institution']
-        profile.country = request.POST['country']
-        profile.position = request.POST['position']
-        profile.grad_date = request.POST['grad_date']
-        profile.brain_structure = request.POST['brain_structure']
-        profile.modalities = request.POST['modalities']
-        profile.methods = request.POST['methods']
-        profile.domain = request.POST['domain']
-        profile.keywords = request.POST['keywords']
-        
-    except (KeyError):
-        # Redisplay the add form.
-        return render(request, 'viewlist/add.html', {
-            'error_message': "Error while submitting the data: %s" % str(KeyError)
-        })
-    else:
-        profile.save();
-        return HttpResponseRedirect(reverse('viewlist:index'))
-    
-def update_profile(request, profile_id):
-    profile = get_object_or_404(Profile, pk=profile_id)
-    try:
-        profile.name = request.POST['name']
-        profile.email = request.POST['email']
-        profile.webpage = request.POST['webpage']
-        profile.institution = request.POST['institution']
-        profile.country = request.POST['country']
-        profile.position = request.POST['position']
-        # profile.grad_date = request.POST['grad_date']
-        profile.brain_structure = request.POST['brain_structure']
-        profile.modalities = request.POST['modalities']
-        profile.methods = request.POST['methods']
-        profile.domain = request.POST['domain']
-        profile.keywords = request.POST['keywords']
-    except (KeyError):
-        # Redisplay the edit form.
-        return render(request, 'viewlist/edit.html', {
-            'profile': profile,
-            'error_message': "Error while submitting the data: %s" % str(KeyError),
-        })
-    else:
-        profile.save();
-        return HttpResponseRedirect(reverse('viewlist:detail', args=(profile.id,)))
-        
-        
+
+class ProfileUpdate(generic.UpdateView):
+    model = Profile
+    fields = [
+        'name', 
+        'email', 
+        'webpage', 
+        'institution',
+        'country',
+        'position',
+        'grad_date',
+        'brain_structure',
+        'modalities',
+        'methods',
+        'domain',
+        'keywords',
+    ]
+
+    def get_success_url(self):
+        return reverse('viewlist:detail', args=(self.object.id,))
+
+class ProfileCreate(generic.CreateView):
+    model = Profile
+    fields = [
+        'name', 
+        'email', 
+        'webpage', 
+        'institution',
+        'country',
+        'position',
+        'grad_date',
+        'brain_structure',
+        'modalities',
+        'methods',
+        'domain',
+        'keywords',
+    ]
+
+    def get_success_url(self):
+            return reverse('viewlist:index')
+  
 def recommend(request, profile_id):
     return HttpResponse("You are writing a recommendation for profile %s." % profile_id)
