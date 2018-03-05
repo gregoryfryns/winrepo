ba6d15bb18b46e97ec00ffdbcebf097c14ba5150
Gregory Fryns <gregory.fryns@gmail.com>
2018-03-05 00:33:57 +0000

    ddd

---
+++ scripts/import_profiles.py
@@ -0,0 +1,51 @@
+from viewlist.models import Profile, Country
+from django.db import IntegrityError
+from django.core.exceptions import ValidationError
+from datetime import datetime
+import csv
+
+incorrect_countries = {}
+with open('scripts/win.tsv', newline='', encoding="utf8") as csvfile:
+    spamreader = list(csv.reader(csvfile, delimiter='\t', quotechar='|'))[1:]
+    for row in spamreader:
+
+        pd = datetime.strptime(row[0]+' UTC', '%d/%m/%Y %H:%M:%S %Z')
+        countryResults = Country.objects.filter(name=row[3])
+        if countryResults:
+            p = Profile(
+                    name = row[1],
+                    institution = row[2],
+                    country = countryResults[0],
+                    email = row[4],
+                    position = row[5],
+                    webpage = row[6],
+                    brain_structure = row[8],
+                    domain = row[9],
+                    modalities = row[10],
+                    methods = row[11],
+                    keywords = row[12],
+                    publish_date = pd.strftime('%Y-%m-%d %H:%M:%S'),
+                    # last_updated = pd.strftime('%Y-%m-%d %H:%M:%S'),
+            )
+
+            if row[7]:
+                gd = row[7].split('/')
+                p.grad_date = '%s-%s-%s' % (gd[2], gd[1], gd[0])
+
+
+            try:
+                p.save()
+            except IntegrityError as ie:
+                print('%s - integrity error: %s' % (row[1], ie))
+            except ValidationError as ve:
+                print('%s - validation error: %s' % (row[1], ve))
+    
+        else:
+            if row[3] in incorrect_countries:
+                incorrect_countries[row[3]] += 1
+            else:
+                incorrect_countries[row[3]] = 1
+                
+print('========= Incorrect countries ==========')
+for key in incorrect_countries:
+    print('%s: (%s occurrences)' % (key, incorrect_countries[key]))
--- viewlist/models.py
+++ viewlist/models.py
@@ -2,43 +2,34 @@ from django.db import models
 from django.utils import timezone
 
 class Country(models.Model):
-    code = models.CharField(max_length=2, blank=False, unique=True)
+    code = models.CharField(max_length=3, blank=False, unique=True)
     name = models.CharField(max_length=60, blank=False)
     is_under_represented = models.BooleanField(default=False)
 
     def __str__(self):
-        return '%s - %s' % (self.code, self.name)
+        return self.name
+        
+    def set_is_under_represented(self, val):
+        self.is_under_represented = val
 
 class Profile(models.Model):
     name = models.CharField(max_length=100, blank=False)
-    email = models.EmailField(unique=True)
-    webpage = models.URLField()
+    email = models.EmailField(unique=True, blank=True)
+    webpage = models.URLField(blank=True)
     institution = models.CharField(max_length=100, blank=False)
     country = models.ForeignKey(Country, on_delete=models.CASCADE)
     position = models.CharField(max_length=30, blank=False)
-    grad_date = models.DateField(null=True)
-    brain_structure = models.CharField(max_length=30)
-    modalities = models.CharField(max_length=50)
-    methods = models.CharField(max_length=50)
-    domain = models.CharField(max_length=30)
-    keywords = models.CharField(max_length=250)
+    grad_date = models.DateField(null=True, blank=True)
+    brain_structure = models.CharField(max_length=80, blank=True)
+    modalities = models.CharField(max_length=80, blank=True)
+    methods = models.CharField(max_length=80, blank=True)
+    domain = models.CharField(max_length=80, blank=True)
+    keywords = models.CharField(max_length=250, blank=True)
     publish_date = models.DateTimeField(default=timezone.now)
-    last_updated = models.DateTimeField(default=timezone.now)
+    last_updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return ' - '.join([self.name, self.position, self.institution])
-
-    def get_search_terms(self):
-        return ' '.join([
-            self.name, 
-            self.institution, 
-            self.country, 
-            self.position, 
-            self.brain_structure, 
-            self.modalities,
-            self.domain,
-            self.keywords
-        ])
 
 class Recommendation(models.Model):
     profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
@@ -47,8 +38,8 @@ class Recommendation(models.Model):
     reviewer_position = models.CharField(max_length=30, blank=False)
     seen_at_conf = models.BooleanField()
     comment = models.CharField(max_length=500, blank=False)
-    publish_date = models.DateTimeField(default=timezone.now)
-    last_updated = models.DateTimeField(default=timezone.now)
+    publish_date = models.DateTimeField(auto_now_add=True)
+    last_updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return "%s said '%s' to %s" % (self.reviewer_name, self.comment, self.profile.name)
