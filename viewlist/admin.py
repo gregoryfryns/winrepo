d2bc852984bcdbf148b094aa421e33f79eab7121
Gregory Fryns <gregory.fryns@gmail.com>
2018-03-17 23:41:03 +0000

    Updated admin views

--- viewlist/admin.py
+++ viewlist/admin.py
@@ -3,6 +3,15 @@ from django.contrib import admin
 # Register your models here.
 from .models import Profile, Recommendation, Country
 
-admin.site.register(Profile)
-admin.site.register(Recommendation)
-admin.site.register(Country)
+class CountryAdmin(admin.ModelAdmin):
+    list_display = ('code', 'name', 'is_under_represented')
+    
+class RecommendationAdmin(admin.ModelAdmin):
+    list_display = ('profile', 'reviewer_name', 'reviewer_email', 'comment')
+    
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('name', 'position', 'institution')
+
+admin.site.register(Profile, ProfileAdmin)
+admin.site.register(Recommendation, RecommendationAdmin)
+admin.site.register(Country, CountryAdmin)
