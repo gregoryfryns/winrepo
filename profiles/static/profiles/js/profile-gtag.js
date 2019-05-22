152ea97850067852f92186be786fe70e30946174
Gregory Fryns <gregory.fryns@gmail.com>
2019-05-22 23:51:03 +0100

    profile-gtag.js script in closure

--- profiles/static/profiles/js/profile-gtag.js
+++ profiles/static/profiles/js/profile-gtag.js
@@ -1,11 +1,13 @@
-$('a.email-btn').click(function() {
-    var emaillink = $(this).attr('href');
-    gtag('event', 'contact', { 'event_category' : 'Contact', 'event_action' : 'Send Email', 'event_label' : emaillink});
-    return true;
-});
+(() => {
+    $('a.email-btn').click(function() {
+        var emaillink = $(this).attr('href');
+        gtag('event', 'contact', { 'event_category' : 'Contact', 'event_action' : 'Send Email', 'event_label' : emaillink});
+        return true;
+    });
 
-$('a.webpage-btn').click(function() {
-    var pageLink =$(this).attr('href');
-    gtag('event', 'contact', { 'event_category' : 'Contact', 'event_action' : 'View Webpage', 'event_label' : pageLink});
-    return true;
-});
+    $('a.webpage-btn').click(function() {
+        var pageLink =$(this).attr('href');
+        gtag('event', 'contact', { 'event_category' : 'Contact', 'event_action' : 'View Webpage', 'event_label' : pageLink});
+        return true;
+    });
+})();
