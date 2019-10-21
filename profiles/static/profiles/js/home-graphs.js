8d826439fd79c68f0c89e649593018affe318155
Gregory Fryns <gregory.fryns@gmail.com>
2019-10-22 00:32:16 +0100

    defer charts drawing

--- profiles/static/profiles/js/home-graphs.js
+++ profiles/static/profiles/js/home-graphs.js
@@ -1,104 +1,104 @@
-function drawPieChart(divId, nbProfiles, totProfiles, label, color1) {
-    const data = new google.visualization.DataTable();
-    data.addColumn('string', 'Position');
-    data.addColumn('number', 'Entries in Repository');
+$(document).ready(() => {
+    function percentString(decimal) {
+        return (decimal * 100).toFixed(0) + '%';
+    }
 
-    data.addRows([
-        [label, nbProfiles],
-        ['Rest', totProfiles - nbProfiles]
-    ]);
+    function drawPieChart(divId, nbProfiles, totProfiles, label, color1) {
+        const array = [['Position', 'Entries in Repository'],
+                        [label, nbProfiles],
+                        ['Rest', totProfiles - nbProfiles]
+                     ];
+        const data = google.visualization.arrayToDataTable(array);
 
-    const options = {
-        colors: ['#dedede', '#dedede'],
-        fontName: 'Open Sans',
-        fontSize: 14,
-        chartArea: { left: '10%', top: 0, width: '80%', height: '100%' },
-        width: 120,
-        height: 120,
-        legend: 'none',
-        pieSliceText: 'none',
-        pieHole: 0.6,
-        enableInteractivity: false,
-        slices: [{ color: color1, textStyle: { color: '#555555', fontName: 'Open Sans', fontSize: 18 } },
-        { textStyle: { color: '#ffffff', fontName: 'Open Sans', fontSize: 1 } }]
-    };
+        const options = {
+            colors: ['#dedede', '#dedede'],
+            fontName: 'Open Sans',
+            fontSize: 14,
+            chartArea: { left: '10%', top: 0, width: '80%', height: '100%' },
+            width: 120,
+            height: 120,
+            legend: 'none',
+            pieSliceText: 'none',
+            pieHole: 0.6,
+            enableInteractivity: false,
+            slices: [{ color: color1, textStyle: { color: '#555555', fontName: 'Open Sans', fontSize: 18 } },
+            { textStyle: { color: '#ffffff', fontName: 'Open Sans', fontSize: 1 } }]
+        };
 
-    const Piechart = new google.visualization.PieChart(document.getElementById(divId));
+        const Piechart = new google.visualization.PieChart(document.getElementById(divId));
 
-    Piechart.draw(data, options);
-}
+        Piechart.draw(data, options);
+    }
 
-function drawMap(divId, jsonData) {
-    const data = new google.visualization.DataTable();
-    data.addColumn('string', 'Country');
-    data.addColumn('number', 'Profiles');
-
-    data.addRows(jsonData.map(country => [country.name, country.profiles_count]));
-    
-    const options = {
-        colorAxis: { colors: ['#a0c1c1', '#10898B'] },
-        legend: 'none',
-        tooltip: { isHtml: false },
-    };
-
-    const chart = new google.visualization.GeoChart(document.getElementById(divId));
-    chart.draw(data, options);
-}
-
-function percentString(decimal) {
-    return (decimal * 100).toFixed(0) + '%';
-}
-
-function init() {
-    $.ajax({
-        url: '/api/countries/?format=json',
-        dataType: "json",
-        success: json => drawMap('regions_div', json)
-    });
-
-    $.ajax({
-        url: '/api/positions/?format=json',
-        dataType: "json",
-        success: json => {
-            let totProfiles = 0;
-            const profiles = {
-                'senior': 0,
-                'postdoc': 0,
-                'student': 0,
-                'other': 0
-            }
-            json.forEach(pos => {
-                totProfiles += pos.profiles_count;
-                if (pos.position.match(/senior|lecturer|professor|director|principal/i)) {
-                    profiles.senior += pos.profiles_count;
-                }
-                else if (pos.position.match(/post-doc/i)) {
-                    profiles.postdoc += pos.profiles_count;
-                }
-                else if (pos.position.match(/phd\sstudent/i)) {
-                    profiles.student += pos.profiles_count;
-                }
-                else {
-                    profiles.other += pos.profiles_count;
-                }
-            });
-
-            $('#student-percent').text(percentString(profiles.student / totProfiles));
-            $('#postdoc-percent').text(percentString(profiles.postdoc / totProfiles));
-            $('#senior-percent').text(percentString(profiles.senior / totProfiles));
-            $('#other-percent').text(percentString(profiles.other / totProfiles));
-
-            $('#student-count').text(profiles.student + ' profiles');
-            $('#postdoc-count').text(profiles.postdoc + ' profiles');
-            $('#senior-count').text(profiles.senior + ' profiles');
-            $('#other-count').text(profiles.other + ' profiles');
-
-            $('#entries-number').text(totProfiles);
-
-            drawPieChart('chartPhD', profiles.student, totProfiles, 'PhD students', '#CC063E');
-            drawPieChart('chartPostDoc', profiles.postdoc, totProfiles, 'Post-doc', '#E83535');
-            drawPieChart('chartSenior', profiles.senior, totProfiles, 'Senior', '#FD9407');
-            drawPieChart('chartOther', profiles.other, totProfiles, 'Other', '#999999');
+    function preparePiecharts(positionsData) {
+        const seniorRe = /senior|lecturer|professor|director|principal/i;
+        const postdocRe = /post-doc/i;
+        const studentRe = /phd\sstudent/i;
+        
+        let totProfiles = 0;
+        const profiles = {
+            'senior': 0,
+            'postdoc': 0,
+            'student': 0,
+            'other': 0
         }
-    });
-}
+
+        for (let i=0; i<positionsData.length; i++) {
+            const pos = positionsData[i];
+            totProfiles += pos.profiles_count;
+            if (pos.position.match(seniorRe)) {
+                profiles.senior += pos.profiles_count;
+            }
+            else if (pos.position.match(postdocRe)) {
+                profiles.postdoc += pos.profiles_count;
+            }
+            else if (pos.position.match(studentRe)) {
+                profiles.student += pos.profiles_count;
+            }
+            else {
+                profiles.other += pos.profiles_count;
+            }
+        };
+
+        $('#student-percent').text(percentString(profiles.student / totProfiles));
+        $('#postdoc-percent').text(percentString(profiles.postdoc / totProfiles));
+        $('#senior-percent').text(percentString(profiles.senior / totProfiles));
+        $('#other-percent').text(percentString(profiles.other / totProfiles));
+
+        $('#student-count').text(profiles.student + ' profiles');
+        $('#postdoc-count').text(profiles.postdoc + ' profiles');
+        $('#senior-count').text(profiles.senior + ' profiles');
+        $('#other-count').text(profiles.other + ' profiles');
+
+        $('#entries-number').text(totProfiles);
+
+        drawPieChart('chartPhD', profiles.student, totProfiles, 'PhD students', '#CC063E');
+        drawPieChart('chartPostDoc', profiles.postdoc, totProfiles, 'Post-doc', '#E83535');
+        drawPieChart('chartSenior', profiles.senior, totProfiles, 'Senior', '#FD9407');
+        drawPieChart('chartOther', profiles.other, totProfiles, 'Other', '#999999');
+    }
+
+    function drawMap(divId, jsonData) {
+        const array = jsonData.map(country => [country.name, country.profiles_count]);
+        array.unshift(['Country', 'Profiles']);
+
+        const data = google.visualization.arrayToDataTable(array);
+
+        const options = {
+            colorAxis: { colors: ['#a0c1c1', '#10898B'] },
+            legend: 'none',
+            tooltip: { isHtml: false },
+        };
+
+        const chart = new google.visualization.GeoChart(document.getElementById(divId));
+        chart.draw(data, options);
+    }
+
+    function init() {
+        $.get('/api/countries/?format=json', data => drawMap('regions_div', data));
+        $.get('/api/positions/?format=json', data => preparePiecharts(data));
+    }
+
+    google.charts.load('current', { 'packages': ['corechart', 'geochart'], 'mapsApiKey': 'AIzaSyCoD-FXcgIKxspIjalcutPYjaSK1B1WmXc' });
+    google.charts.setOnLoadCallback(init);
+});
