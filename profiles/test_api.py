f2e2447c0c778bcaa983586418e93af76a981cc8
Gregory Fryns <gregory.fryns@gmail.com>
2019-11-13 23:49:49 +0000

    test api recommendations

--- profiles/test_api.py
+++ profiles/test_api.py
@@ -28,7 +28,7 @@ class ProfileViewSetTestCase(APITestCase):
 
     list_url = reverse('profiles:profiles-list')
 
-    def setup(self):
+    def setUp(self):
         self.user = User.objects.create_user(username='testcase', email='test@test.com', password='my_super_password')
         self.token = Token.objects.create(user=self.user)
         self.api_authentication()
@@ -47,23 +47,54 @@ class ProfileViewSetTestCase(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_profile_detail_retrieve(self):
-        response = self.client.get(reverse('profiles:profiles-detail', kwargs={'pk': 1}))
+        profile_id = self.user.profile.pk
+        response = self.client.get(reverse('profiles:profiles-detail', kwargs={'pk': profile_id}))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data['user'], 'testcase')
 
     def test_profile_update_by_owner(self):
-        test_data = {'institution': 'Test Uni', 'name': 'Test Name', 'Keywords': 'Updated!'}
-        response = self.client.put(reverse('profiles:profiles-detail', kwargs={'pk': 1}),
+        test_data = {'institution': 'Test Uni', 'name': 'Test Name', 'keywords': 'Updated!'}
+        profile_id = self.user.profile.pk
+        response = self.client.put(reverse('profiles:profiles-detail', kwargs={'pk': profile_id}),
                                    test_data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        data = json.loads(response.content())
-        for k, v in test_data:
+        data = json.loads(response.content)
+        for k, v in test_data.items():
             self.assertEqual(data[k], v)
 
     def test_profile_update_by_other_user(self):
+        profile_id = self.user.profile.pk
         random_user = User.objects.create_user(username='random', email='random@test.com', password='my_super_password')
         self.client.force_authenticate(user=random_user)
 
-        response = self.client.put(reverse('profiles:profiles-detail', kwargs={'pk': 1}),
-                                   {'institution': 'Test Uni', 'name': 'Random Name', 'Keywords': 'Hacked!'})
+        response = self.client.put(reverse('profiles:profiles-detail', kwargs={'pk': profile_id}),
+                                   {'institution': 'Test Uni', 'name': 'Random Name', 'keywords': 'Hacked!'})
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+
+class RecommendationViewSetTestClass(APITestCase):
+
+    list_url = reverse('profiles:recommendations-list')
+
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='my_super_password')
+        self.recommendation = Recommendation.objects.create(profile=self.user.profile,
+                                                            reviewer_name='Test Reviewer',
+                                                            reviewer_email='test@reviewer.com',
+                                                            reviewer_institution='Test institution',
+                                                            seen_at_conf=False,
+                                                            comment='Awesome!')
+        self.token = Token.objects.create(user=self.user)
+        self.api_authentication()
+
+    def api_authentication(self):
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+
+    def test_recommendations_list_authenticated(self):
+        response = self.client.get(self.list_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_recommendations_list_unauthenticated(self):
+        self.client.force_authenticate(user=None)
+        response = self.client.get(self.list_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
